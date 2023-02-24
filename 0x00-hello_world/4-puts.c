dcd91d47d5fb2fe19aa27f54df041ae0c067baf8
giltonia/alx-pre_course.git <giltonia>
2023-02-24 05:04:49 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include <stdoi.h>
+#include <stdio.h>
 /**
-  * main - A programme that prints a line with puts function
- * Return: 0 (Successful)
+ *  main - A programme that prints a line with  puts function
+ *  Return: (0) (Success)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
-
