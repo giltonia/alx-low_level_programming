d043443ac06351e9dfa5a929b312c00f2bc97278
giltonia/alx-pre_course.git <giltonia>
2023-03-05 12:29:56 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
 /**
- * Time_table - prints 9 times table starting with 0
- *
- * Return: empty output
+ * times_table - prints the 9 times table, starting with 0
  */
-void time_table(void)
+void times_table(void)
 {
 	int i, j, k;
 
