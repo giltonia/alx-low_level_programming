086e91b0192adcaad71a30d2e7e9c062f67d2026
giltonia/alx-pre_course.git <giltonia>
2023-03-06 02:59:26 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,33 +1,26 @@
 #include "main.h"
+
 /**
-* print_triangle - this function ptints a triangle
-* @size: parameter needed
-* Return: 0
-*/
-void print_triangle(int size)
+ * print_number - prints an integer
+ * @n: integer to be printed
+ */
+void print_number(int n)
 {
-int i = 0, j, n = size - 1;
-if (size > 0)
-{
-for (; i < size; i++)
-{
-for (j = 0; j < size; j++)
-{
-if (j < n)
-{
-_putchar(' ');
-}
-else
-{
-_putchar('#');
-}
-}
-n--;
-_putchar('\n');
-}
-}
-else
-{
-_putchar('\n');
-}
-}
+	unsigned int n1;
+
+	if (n < 0)
+	{
+		n1 = -n;
+		_putchar('-');
+	} else
+	{
+		n1 = n;
+	}
+
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
+	_putchar((n1 % 10) + '0');
+
