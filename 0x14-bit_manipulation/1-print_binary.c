235be383d9a26a986a224703936db2561995b924
giltonia <gsammy24560@gmail.com>
2023-08-03 12:48:22 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_binary - pints the binay equivalent of a decimal
+ * @n: number to print
+ *
+ * Return: ...
+ */
+
+void print_binary(unsigned long int n)
+{
+	int k, count = 0;
+	unsigned long int current;
+
+	for (k = 63; k >= 0; k--)
+	{
+		current >> k;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+
+		else if (count)
+			_putchar('0');
+	}
+
+	if (!count)
+		_putchar('0');
+}
